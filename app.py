45fa5053f3a808bb538811ce16a5a72d610b9d08
Sam Cheng <samchenghowing@gmail.com>
2024-03-18 17:09:08 +0800

    getDriver isRapidlySpeedup api added

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-import argparse
-from flask import Flask
+# https://docs.aws.amazon.com/emr/latest/ManagementGuide/emr-gs.html
 
+import argparse
+from os import walk
 from pyspark.sql import SparkSession
+
+from flask import Flask, request, jsonify, send_file
 
 # DEFAULT_DATA_SOURCE = "s3://comp4442project2024spring/detail-records/detail_record_2017_01_02_08_00_00"
 # DEFAULT_OUTPUT_URL  = "s3://comp4442project2024spring/result/csv"
@@ -18,6 +21,36 @@ def index():
 def hello():
     calculate_rapidly_speedup()
     return "Hello World!"
+
+@application.route("/getDriverByID", methods=['POST'])
+def getDriverByID():
+    json_data = request.get_json()
+    driverID = json_data['driverID']
+    
+    data_source="./detail-records/detail_record_2017_01_02_08_00_00"
+
+    with SparkSession.builder.appName("Calculate rapidly speedup").getOrCreate() as spark:
+        # Load the restaurant violation CSV data
+        if data_source is not None:
+            df = spark.read \
+                    .csv(data_source, inferSchema=True) \
+                    .toDF("driverID","carPlateNumber","Latitude","Longtitude","Speed","Direction","siteName","Time", \
+                          "isRapidlySpeedup","isRapidlySlowdown","isNeutralSlide","isNeutralSlideFinished","neutralSlideTime", \
+                            "isOverspeed","isOverspeedFinished","isOverspeedFinished","overspeedTime","isFatigueDriving","isHthrottleStop","isOilLeak")
+
+        # Create an in-memory DataFrame to query
+        df.createOrReplaceTempView("rapidly_speedup")
+
+        # Create a DataFrame of the top 10 restaurants with the most Red violations
+        top_rapidly_speedup = spark.sql("""SELECT count(*) 
+          FROM rapidly_speedup 
+          WHERE isRapidlySpeedup = '1' and driverID = {DID}
+          """, DID=driverID)
+        
+        total_speedup_count = top_rapidly_speedup.collect()
+        json_data = {"driverID": driverID, "total_speedup_count": total_speedup_count}
+
+    return jsonify(json_data), 200
 
 def calculate_rapidly_speedup(data_source=DEFAULT_DATA_SOURCE, output_uri=DEFAULT_OUTPUT_URL):
     """
