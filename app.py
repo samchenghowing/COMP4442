b361cf75dde1d9fe6ec7fcd16ae11d1b35a87282
Sam Cheng <samchenghowing@gmail.com>
2024-04-19 23:04:50 +0800

    bug fix

--- app.py
+++ app.py
@@ -1,172 +1,204 @@
+from flask import Flask, request, jsonify, render_template
 import json
-from datetime import datetime, timedelta
-from flask import Flask,request,jsonify, session
-from flask_cors import CORS, cross_origin
-from flask_socketio import emit, join_room, leave_room, SocketIO
-import sqlite3
-from werkzeug.security import generate_password_hash, check_password_hash
+import mysql.connector
+import datetime, time 
 
-# Client IPDict
-IPDict = {}
 
-app = Flask(__name__)
-cors = CORS(app)
-app.config.update(DEBUG=True, CORS_HEADERS='Content-Type')
-socketio = SocketIO(app, cors_allowed_origins="*", engineio_logger=True)
+# to be moved to driver_summary.py
+# #############
+# https://docs.aws.amazon.com/emr/latest/ManagementGuide/emr-gs.html
+import argparse
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+debugMode = True
+if debugMode:
+    DEFAULT_DATA_SOURCE = "./detail-records"
+    DEFAULT_OUTPUT_URL  = "./result/csv"
+else:
+    DEFAULT_DATA_SOURCE = "s3://comp4442project2024spring/detail-records"
+    DEFAULT_OUTPUT_URL  = "s3://comp4442project2024spring/result/csv"
+schema = StructType() \
+      .add("driverID",StringType(),True) \
+      .add("carPlateNumber",StringType(),True) \
+      .add("Latitude",DoubleType(),True) \
+      .add("Longtitude",DoubleType(),True) \
+      .add("Speed",IntegerType(),True) \
+      .add("Direction",IntegerType(),True) \
+      .add("siteName",StringType(),True) \
+      .add("Time",DateType(),True) \
+      .add("isRapidlySpeedup",IntegerType(),True) \
+      .add("isRapidlySlowdown",IntegerType(),True) \
+      .add("isNeutralSlide",IntegerType(),True) \
+      .add("isNeutralSlideFinished",IntegerType(),True) \
+      .add("neutralSlideTime",IntegerType(),True) \
+      .add("isOverspeed",IntegerType(),True) \
+      .add("isOverspeedFinished",IntegerType(),True) \
+      .add("overspeedTime",IntegerType(),True) \
+      .add("isFatigueDriving",IntegerType(),True) \
+      .add("isHthrottleStop",IntegerType(),True) \
+      .add("isOilLeak",IntegerType(),True)
+# #############
+# end to be moved to driver_summary.py
 
-def dict_factory(cursor, row):
-    d = {}
-    for idx, col in enumerate(cursor.description):
-        d[col[0]] = row[idx]
-    return d
 
-def get_db_connection():
-    conn = sqlite3.connect('database.db')
-    conn.row_factory = dict_factory
-    return conn
+application = Flask(__name__)
 
-def checkpassword(name, password):
-    conn = get_db_connection()
-    sql_select_query = """select * from users where name = ?"""
-    user = conn.execute(sql_select_query, (name,)).fetchone()
-    conn.close()
+def db_connection():
+    mydb = mysql.connector.connect( host = 'database-1.cti2wk8aib5l.us-east-1.rds.amazonaws.com',
+    user = 'admin',
+    port = '3306',
+    database = 'lab6',
+    passwd = '12345678',
+    autocommit = True)
 
-    if user is None:
-        json_data = {"isvalid":False,"from client": request.remote_addr,
-                        "attempt count": IPDict[request.remote_addr][0],
-                        "status": "User not exist in database!"}
-        return json_data
-
-    hashedpw = user.get('pwHash')
-    if check_password_hash(hashedpw, password):
-        # update the format of hash before send to client
-        user["pwHash"] = password
-        IPDict[request.remote_addr].append(user.get('id'))
-        json_data = {"isvalid":True, "from client": request.remote_addr,
-                    "attempt count": IPDict[request.remote_addr][0], "User info": user}
-        return json_data
-    else:
-        json_data = {"isvalid":False, "from client": request.remote_addr,
-                    "attempt count": IPDict[request.remote_addr][0], "status": "password not match"}
-        return json_data
-
-# This route is for testing use only.
-@app.route("/")
-def main():
-    return "Wellcome to COMP3334 Backend!" 
-
-# This route is for testing use only.
-@app.route('/api/users')
-@cross_origin()
-def get_users():
-    conn = get_db_connection()
-    users = conn.execute('SELECT * FROM users').fetchall()
-    conn.close()
-    return users, 200
-
-@app.route('/api/signup', methods=['POST'])
-@cross_origin()
-def signup():
-    json_data = request.get_json()
-    name = json_data['name']
-    password = json_data['pwHash']
-
-    # check if user exist in database
-    conn = get_db_connection()
-    sql_select_query = """select * from users where name = ?"""
-    res = conn.execute(sql_select_query, (name,)).fetchone()
-    if res is not None:
-        json_data = {"signup":False, "status": "User alreafy exist in database!"}
-        return jsonify(json_data), 200
-
-    # TO-DO: email verify
-
-    hash = generate_password_hash(password)
-    conn.execute("INSERT INTO users (name, pwHash, confirmed) VALUES (?, ?, ?)",
-                (name, hash, 0)
-                )
+    #print("successfully connect to the database")
     
-    conn.commit()
-    conn.close()
+    return mydb
 
-    json_data = {"signup":True}
+mydb = db_connection()
+cur = mydb.cursor()
+
+@application.route("/")
+def index():
+    return render_template("monitor.html")
+
+@application.route("/hello")
+def hello():
+    # testing
+    json_data = getDriverSummary("hanhui1000002", "2017-01-01 00:00:00", "2017-01-02 00:00:00")
     return jsonify(json_data), 200
 
-@app.route('/api/login', methods=['POST'])
-@cross_origin()
-def login():
-
-    # check if brute force trying password to login
-    if request.remote_addr not in IPDict:
-        IPDict[request.remote_addr] = [0, datetime.now()]
-    else:
-        # reset the count to unblock user after 5 minutes
-        if datetime.now() - IPDict[request.remote_addr][1] > timedelta(minutes=5):
-            IPDict[request.remote_addr] = [0, datetime.now()]
-
-        if IPDict[request.remote_addr][0] == 5:
-            json_data = {"isvalid":False, "from client": request.remote_addr, 
-                          "attempt count": IPDict[request.remote_addr][0], 
-                          "status": "This IP is blocked 5 minutes since too many\
-                                    failed login attempts"}
-            return jsonify(json_data), 200
-        IPDict[request.remote_addr][0] = IPDict[request.remote_addr][0] + 1
-
+@application.route("/api/getDriverByID", methods=['POST'])
+def getDriverByID():
     json_data = request.get_json()
-    name = json_data['name']
-    password = json_data['pwHash']
+    driverID = json_data['driverID']
 
-    json_data = checkpassword(name, password)
+    json_data = getDriverSummary(driverID, "2017-01-01 00:00:00", "2017-01-02 00:00:00")
     return jsonify(json_data), 200
 
-    # check if brute force trying password to login
-    if request.remote_addr not in IPDict:
-        IPDict[request.remote_addr] = [0, datetime.now()]
-    else:
-        # reset the count to unblock user after 5 minutes
-        if datetime.now() - IPDict[request.remote_addr][1] > timedelta(minutes=5):
-            IPDict[request.remote_addr] = [0, datetime.now()]
+def getDriverSummary(driverID, startDate, endDate, data_source=DEFAULT_DATA_SOURCE, output_uri=DEFAULT_OUTPUT_URL):
+    """
+    a) Generate a summary to show the driving behavior of each driver. 
+    You are required to display the driving behavior information during the given period in a HTML table.
+    The information includes but not limited to the car plate number, the cumulative 
+    number of times of overspeed and fatigue driving, the total time of overspeed and neutral slide.
 
-        if IPDict[request.remote_addr][0] == 5:
-            json_data = {
-                "isvalid":False, "from client": request.remote_addr, 
-                "attempt count": IPDict[request.remote_addr][0], 
-                "status": "This IP is blocked 5 minutes since too many\
-                    failed login attempts"}
-            return jsonify(json_data), 200
-        IPDict[request.remote_addr][0] = IPDict[request.remote_addr][0] + 1
+    :param driverID: The requested ID of the driver
+    :param startDate: The given start date driving period
+    :param endDate: The given end date driving period
+    :param data_source: The URI of your food establishment data CSV, such as 's3://DOC-EXAMPLE-BUCKET/food-establishment-data.csv'.
+    :param output_uri: The URI where output is written, such as 's3://DOC-EXAMPLE-BUCKET/restaurant_violation_results'.
+    """
+    with SparkSession.builder.appName("Generate driver summary").getOrCreate() as spark:
+        if data_source is not None:
+            df = spark.read.format("csv") \
+                        .option("header", False) \
+                        .schema(schema) \
+                        .load(data_source)
 
-    json_data = request.get_json()
+        # Create an in-memory DataFrame to query
+        df.createOrReplaceTempView("driver_summary")
 
-    json_response = checkpassword(json_data['name'], json_data['pwHash'])
-    return json_response, 200
+        distinct_driver_ids = df.select(col('driverID')).distinct()
 
-@socketio.on('joined', namespace='/chat')
-def joined(message):
-    """Sent by clients when they enter a room.
-    A status message is broadcast to all people in the room."""
-    data_dict = json.loads(message["msg"])
-    room = int(data_dict["room"])
-    join_room(room)
-    emit('status', {'msg': data_dict["name"] + ' has entered the room.'}, room=room)
+        # Show the distinct driverID values
+        distinct_driver_ids.show()
 
-@socketio.on('text', namespace='/chat')
-def text(message):
-    """Sent by a client when the user entered a new message.
-    The message is sent to all people in the room."""
-    data_dict = json.loads(message["msg"])
-    room = int(data_dict["room"])
-    emit('message', {'msg': message}, room=room)
+        # get carPlateNumber
+        carPlateNumber_result = spark.sql("""
+        SELECT carPlateNumber
+            FROM driver_summary
+            WHERE driverID = {DID}
+        """, DID=driverID)
+        carPlateNumber = carPlateNumber_result.first().carPlateNumber
 
-@socketio.on('left', namespace='/chat')
-def left(message):
-    """Sent by clients when they leave a room.
-    A status message is broadcast to all people in the room."""
-    data_dict = json.loads(message["msg"])
-    room = int(data_dict["room"])
-    leave_room(room)
-    emit('status', {'msg': data_dict["name"] + ' has left the room.'}, room=room)
+        # get cumulative number of overspeed
+        cumulative_overspeed_result = spark.sql("""
+        SELECT COUNT(*) AS cumulative_overspeed_count
+            FROM driver_summary
+            WHERE isOverspeed = 1 AND driverID = {DID} AND Time >= {start} AND Time <= {end}
+        """, DID=driverID, start=startDate, end=endDate)
+        cumulative_overspeed_count = cumulative_overspeed_result.first().cumulative_overspeed_count
 
+        # get cumulative number of fatigue driving
+        cumulative_fatigue_result = spark.sql("""
+            SELECT COUNT(*) AS cumulative_fatigue_count
+            FROM driver_summary
+            WHERE isFatigueDriving = 1 AND driverID = {DID} AND Time >= {start} AND Time <= {end}
+        """, DID=driverID, start=startDate, end=endDate)
+        cumulative_fatigue_count = cumulative_fatigue_result.first().cumulative_fatigue_count
+
+        # # get total time of overspeed
+        total_overspeed_time_result = spark.sql("""
+            SELECT SUM(overspeedTime) AS total_overspeed_time
+            FROM driver_summary
+            WHERE driverID = {DID} AND Time >= {start} AND Time <= {end}
+        """, DID=driverID, start=startDate, end=endDate)
+        total_overspeed_time = total_overspeed_time_result.first().total_overspeed_time
+
+        # # get total time of neutral slide
+        total_neutral_slide_time_result = spark.sql("""
+            SELECT SUM(neutralSlideTime) AS total_neutral_slide_time
+            FROM driver_summary
+            WHERE driverID = {DID} AND Time >= {start} AND Time <= {end}
+        """, DID=driverID, start=startDate, end=endDate)
+        total_neutral_slide_time = total_neutral_slide_time_result.first().total_neutral_slide_time
+
+        # get cumulative number of hthrottle stop
+        cumulative_hthrottle_stop_result = spark.sql("""
+            SELECT COUNT(*) AS cumulative_hthrottle_stop_count
+            FROM driver_summary
+            WHERE isHthrottleStop = 1 AND driverID = {DID} AND Time >= {start} AND Time <= {end}
+        """, DID=driverID, start=startDate, end=endDate)
+        cumulative_hthrottle_stop_count = cumulative_hthrottle_stop_result.first().cumulative_hthrottle_stop_count
+
+        # get cumulative number of oil leak
+        cumulative_oil_leak_result = spark.sql("""
+            SELECT COUNT(*) AS cumulative_oil_leak_count
+            FROM driver_summary
+            WHERE isOilLeak = 1 AND driverID = {DID} AND Time >= {start} AND Time <= {end}
+        """, DID=driverID, start=startDate, end=endDate)
+        cumulative_oil_leak_count = cumulative_oil_leak_result.first().cumulative_oil_leak_count
+
+
+        json_data = {
+            "driverID": driverID,
+            "carPlateNumber": carPlateNumber,
+            "cumulative_overspeed_count": cumulative_overspeed_count,
+            "cumulative_fatigue_count": cumulative_fatigue_count,
+            "total_overspeed_time": total_overspeed_time,
+            "total_neutral_slide_time": total_neutral_slide_time,
+            "cumulative_hthrottle_stop_count": cumulative_hthrottle_stop_count,
+            "cumulative_oil_leak_count": cumulative_oil_leak_count,
+            "endDate": endDate
+        }
+        return json_data
+
+tmp_time = 0
+@application.route("/data")
+def getdata():
+	# global tmp_time
+	# if tmp_time > 0 :
+	# 	sql = "select ctime,num from Monitor where ctime >%s" %(tmp_time)
+	# else:
+    #     s = "01/01/2017"
+    #     tmp_time = time.mktime(datetime.datetime.strptime(s, "%d/%m/%Y").timetuple())
+	# 	sql = "select ctime,num from Monitor"
+
+	# cur.execute(sql)
+    json_data = getDriverSummary("hanhui1000002", "2017-01-01 00:00:00", "2017-01-02 00:00:00")
+    tmp_time = json_data['endDate']
+    return jsonify(json_data), 200
+
+	# datas = []
+	# for i in cur.fetchall():
+	# 	datas.append([i[0], i[1]])
+
+	# if len(datas) > 0 :
+	# 	tmp_time = datas[-1][0]
+
+	# return json.dumps(datas)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5050)
+    application.run(port=5000, debug=debugMode, CORS_HEADERS='Content-Type')
